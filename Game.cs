2782e82bee3679fec034b776d77741d2accf5387
Judeous <judeous@tutanota.com>
2020-08-19 11:40:48 -0500

    Added spacing and a note

--- Game.cs
+++ Game.cs
@@ -14,12 +14,14 @@ namespace HelloWorld
             Console.WriteLine("What is your name? ");
             Console.Write("My name is ");
             string name = Console.ReadLine(); //Gets the player's name
+            Console.WriteLine("");
 
             Console.Write("Welcome, " + name);
             Console.WriteLine(", what is your class?");
 
             Console.Write("My class is ");
             string role = Console.ReadLine();
+            Console.WriteLine("");
 
             bool maxLevelReached = false; //Checks to see if the player is on the last level, and sets it so the player doesn't start on the last level
             int maxLevel = 100; //Sets the last level so the original thing knows what the last level is
@@ -27,10 +29,11 @@ namespace HelloWorld
 
             bool ready = true;
 
-            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Name: " + name); //This and next three lines are just to show to the player their stats
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Level: " + level);
             Console.WriteLine("Class: " + role);
+
 
         }
     }
